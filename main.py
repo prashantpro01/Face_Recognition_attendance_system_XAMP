0a1e8a84c0fa62c8c06e4212413d7bcf92b5608c
Prashant <ppy.ppy.1997@gmail.com>
2022-08-07 23:11:38 +0530

    cleaned unnecessary code

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def main():
             encode =face_recognition.face_encodings(img)[0]
             encodeList.append(encode)
         return encodeList
-
     encodeListKnown = faceEncodings(images)
     print("encoding done") 
 
@@ -69,14 +68,12 @@ def main():
                 cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
                 cv2.rectangle(frame,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
                 cv2.putText(frame,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
-                print(count)
                 if count == 1 :
                     print(name)
                     count=0  
                     A = messagebox.askyesno('HELLO '+name, 'click yes to mark your attendance')  
                     print(A) 
                     if A == True :
-                        print("hi")
                         conn = connect(host="localhost",user="root",password="",db='FRBAS')
                         var = conn.cursor()
                         var.execute("insert into attendance (name,date,time) values ('"+name+"',curdate(),curtime())")
